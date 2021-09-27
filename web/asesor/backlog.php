e81159f8dd7337381ded997676c3d045124d629a
andres2021alvarez <andres.alvarez@esinergia.co>
2021-09-27 07:38:49 -0500

    cambios ventas asesor

--- web/asesor/backlog.php
+++ web/asesor/backlog.php
@@ -35,6 +35,10 @@
   <link rel="stylesheet" href="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+   <!-- DataTables -->
+   <link rel="stylesheet" href="../plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="../plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="../plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 </head>
 <body class="hold-transition  sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 <div class="wrapper">
@@ -344,6 +348,19 @@
 <script src="../plugins/raphael/raphael.min.js"></script>
 <script src="../plugins/jquery-mapael/jquery.mapael.min.js"></script>
 <script src="../plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="../plugins/jszip/jszip.min.js"></script>
+<script src="../plugins/pdfmake/pdfmake.min.js"></script>
+<script src="../plugins/pdfmake/vfs_fonts.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 <!-- ChartJS -->
 <script src="../plugins/chart.js/Chart.min.js"></script>
 
@@ -351,6 +368,32 @@
 <script src="../dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="../dist/js/pages/dashboard2.js"></script>
+<script>
+  $(function () {
+   
+    $('#list_usuarios').DataTable({
+        responsive:true,
+        dom: 'Bfrtip',
+        lengthMenu: [
+            [ 10, 25, 50, -1 ],
+            [ '10 ', '25 ', '50 ', 'Mostrar todo' ]
+        ],
+        buttons: [
+            'pageLength',
+            
+        ],
+        "language": {
+            "url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json",
+            buttons: {
+                pageLength: {
+                    _: "Mostrando %d Registros",
+                    '-1': "Mostrar todo"
+                }
+            }
+        }
+    });
+  });
+</script>
 <script>
     async function fetch_form(data) {      
     try {
